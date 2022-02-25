5081d130bbbe90509d1e87b3e27a25c6b6b86ab2
channacy <68759286+channacy@users.noreply.github.com>
2022-02-24 21:49:42 -0500

    option cards can add to deck

--- Board.gd
+++ Board.gd
@@ -43,29 +43,23 @@ func execute(effectData):
 						cardsToRemove.append(card)
 		for card in cardsToRemove:
 			deck.cards.erase(card)
-			
+	
 	#When an option card that adds cards is selected, it adds the correct cards to the deck
+	
 	#Cards can be added to the deck by id
-	if len(effectData.addId) > 0:
-		var cardsToAdd = []
-		for card in deck.cards:
-			for id in effectData.addId:
-				if(Global.files[card].id == id):
-					cardsToAdd.append(card)
-		for card in cardsToAdd:
-			deck.cards.add(card)
+	if len(effectData.addId) > 0: #if there is an effect added to the array based on Id
+		for id in effectData.addId:  #iterates through the array 
+			for card in Global.files:  #search through each card in dictionary 
+				if (id == Global.files[card].id): #if something in the array equals something in the dictionary
+					deck.cards.append(id)  #add this to the end of the deck which is an array of strings
+			
 	#Cards can be added to the deck by tag
-	if len(effectData.addTag) > 0:
-		var cardsToAdd = []
-		for card in deck.cards:
-			for tag in effectData.addTag:
-				for cardTag in Global.files[card].tags:
-					if cardTag == tag:
-						cardsToAdd.append(card)
-		for card in cardsToAdd:
-			deck.cards.add(card)
-		
-
+	if len(effectData.addTag) > 0:  #if there is an effect added to the array based on Id
+		for card in Global.files:  #search through dictionary
+			for tag in effectData.addTag:  #search through each tag in the array 
+				for cardTag in Global.files[card].tags: #search through each tag in the array of tags
+					if cardTag == tag:  #if the tag in the array of tags that is within the dictionary equals tag in effectData.addTag
+						deck.cards.append(Global.files[card].id) #add it to the deck which is an array of strings
 
 # Returns true if there is an active event card on the board
 func hasEventCard():

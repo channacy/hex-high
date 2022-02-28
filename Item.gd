a5623179bae756785eafe19202e4157bf07a7961
ag3985 <ag3985@drexel.edu>
2022-02-28 00:46:16 -0500

    added item comments

--- Item.gd
+++ Item.gd
@@ -1,11 +1,10 @@
 extends Area2D
 class_name Item
 
-# Called when the node enters the scene tree for the first time.
+
+#creates inventory object
 func setup(item, numOfItems):
 	$Sprite.texture = item.itemImg
 	$NumberContainer/Number.bbcode_text = str(numOfItems)
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
--- ItemData.gd
+++ ItemData.gd
@@ -1,15 +1,13 @@
 extends Resource
 class_name ItemData
 
+#labels for inventory items
 export(String) var id = ""
 
+#displays inventory item image
 export(Texture) var itemImg = load("res://sprites/defaultFront.png")
 
+#label for amount of items held
 export(int) var numOfItems = 0
 
-# Called when the node enters the scene tree for the first time.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass

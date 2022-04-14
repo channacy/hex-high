dbb2679d3c904b40494b64b8ed5af3ae4f446695
ag3985 <ag3985@drexel.edu>
2022-04-14 12:48:09 -0400

    implemented tutorial

---
+++ tutorial/back1.gd
@@ -0,0 +1,9 @@
+extends Button
+
+
+
+
+
+func _on_back_pressed():
+	Global.goto_scene("res://Menu.tscn")
+
---
+++ tutorial/forward2.gd
@@ -0,0 +1,7 @@
+extends Button
+
+
+
+
+func _on_forward_pressed():
+	Global.goto_scene("res://tutorial/slide3.tscn")

ef61b42df4b19349df751424185165ecbf92ed1c
Channacy <cu62@drexel.edu>
2022-05-23 01:03:40 -0400

    changed amount of fatigue gained for some cards and created fallAsleep card

--- Inventory.gd
+++ Inventory.gd
@@ -70,19 +70,27 @@ func updateResourceConditionals():
 			deck.cards.erase("writeupsPenalty")
 			
 	#add scholarship
-	if inventoryItems["alchemy"] >= 25 and inventoryItems["summon"] >= 25 and inventoryItems["sorcery"] >= 25 and inventoryItems["artifice"] >= 25:
+	if inventoryItems["alchemy"] >= 25 and inventoryItems["summon"] >= 25 and inventoryItems["sorcery"] >= 25 and inventoryItems["artifice"] >= 25 and inventoryItems["coin"] < 25:
 		deck.cards.append("scholarship")
 	#remove scholarship
-	if inventoryItems["alchemy"] < 25 and inventoryItems["summon"] < 25 and inventoryItems["sorcery"] < 25 and inventoryItems["artifice"] < 25:
+	if inventoryItems["alchemy"] < 25 and inventoryItems["summon"] < 25 and inventoryItems["sorcery"] < 25 and inventoryItems["artifice"] < 25 and inventoryItems["coin"] >= 50:
 		while deck.cards.find("scholarship") != -1:
 			deck.cards.erase("scholarship")
 			
+			
 	#add social
-	if inventoryItems["fatigue"] >= 10:
+	if inventoryItems["fatigue"] >= 6:
 		deck.cards.append("social")
 	#remove social
-	if inventoryItems["fatigue"] < 10:
+	if inventoryItems["fatigue"] < 6:
 		while deck.cards.find("social") != -1:
 			deck.cards.erase("social")
 			
+	#add fallAsleep
+	if inventoryItems["fatigue"] >= 9:
+		deck.cards.append("fallAsleep")
+	#remove fallAsleep
+	if inventoryItems["fatigue"] < 9:
+		while deck.cards.find("fallAsleep") != -1:
+			deck.cards.erase("fallAsleep")
 	
